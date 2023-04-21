fecc2927215a37adb18a1fbbe9af68f77856c8c7
Joshua Eichhorn <JoshuaEichhorn@ymail.com>
2023-04-21 17:28:03 -0400

    creates dynamic animations for questions

--- src/Lab.js
+++ src/Lab.js
@@ -3,11 +3,11 @@ import LabQuestionsContainer from './LabQuestionsContainer'
 
 function Lab() {
   return (
-    <div id="home-container">
-      <div id="home-left"> 
+    <div id="lab-container">
+      <div id="lab-left"> 
             <h1>Place Holder Lab</h1>
         </div>
-        <div id="home-right">
+        <div id="lab-right">
           <LabQuestionsContainer />
         </div>
     </div>
--- src/LabQuestionsCard.js
+++ src/LabQuestionsCard.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function LabQuestionsCard() {
+function LabQuestionsCard({showQuestions}) {
+
+const [click, setClick] = useState(false)
+
   return (
     <div>
       <h2>This is a sample question.</h2>
-      <button>Answer</button>
-      <button>Answer</button>
+      <button disabled={click} 
+        onClick={() => {
+        showQuestions()
+        setClick(true)}}>Answer</button>
+      <button disabled={click} 
+        onClick={() => {
+        showQuestions()
+        setClick(true)}}>Answer</button>
     </div>
   )
 }
