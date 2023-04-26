1f6602963a44a3a090fe719954dff10b4be6bedd
rknm-cell <racknahm@gmail.com>
2023-04-26 10:46:35 -0400

    ui changes

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button, ButtonGroup, Typography } from "@mui/material";
+
+// changing ui
 function Home() {
   return (
     <div id="home-container">
