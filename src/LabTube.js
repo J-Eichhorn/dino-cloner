49d837d045006be759b63b33cdbe4ff191fedc00
rknm-cell <racknahm@gmail.com>
2023-04-27 11:16:45 -0400

    updated image assets, added bubble animation, and css for bubbles

--- src/LabTube.js
+++ src/LabTube.js
@@ -2,21 +2,20 @@ import React, { useState } from "react";
 import Bubbles from "./Bubbles";
 
 function LabTube() {
-
-  
-  
-  
   return (
-    <div>
-      
-      <div><img
+    <div style={{position: "absolute"}}>
+      <img
         style={{ width: 500 }}
         src="../images/test_tube.png"
         alt="test-tube"
-      ></img><Bubbles /></div>
-      
+      ></img>
+      <img
+        style={{ width: 100, position: "absolute", left: 200, top: 48 }}
+        src="../images/bubbles.gif"
+        alt="bubbles"
+      ></img>
+      <div style={{ position: "absolute", width: 500 }}></div>
     </div>
-    
   );
 }
 
